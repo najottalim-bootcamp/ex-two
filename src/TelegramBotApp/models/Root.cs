c23ad111788abeb6e5a237556e0cb740ec491108
FarxodbekMadrahimov <farhodbekmadrahimov557@gmail.com>
2023-10-03 20:25:50 +0500

    models done

---
+++ src/TelegramBotApp/models/Root.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBotApp.models;
+
+namespace TelegramBotApp.Farxodbek
+{
+    public class Root
+    {
+       
+            public List<Search> Search { get; set; }
+            public string totalResults { get; set; }
+            public string Response { get; set; }
+      
+
+
+    }
+}
