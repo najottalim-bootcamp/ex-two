77ad8acbd17fed4792dbf63bff94fa599506b2b7
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-04 11:54:57 +0500

    sending films

---
+++ src/TelegramBotApp/Service/BotService.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TelegramBotApp.Service
+{
+    public class BotService
+    {
+        public static async Task SendFilmsAsync(ITelegramBotClient botClient, Message? message, CancellationToken cancellationToken)
+        {
+            FilmService filmService = new FilmService();
+            var filmList = await filmService.GetFilmListAsync(message.Text);
+            var replyMarkup = new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("text"));
+            await botClient.SendTextMessageAsync(
+                chatId: message.From.Id,
+                text: $"Search:{filmList.SearchKey} page-{filmList.pageIndex}",
+                cancellationToken: cancellationToken);
+        }
+    }
+}
