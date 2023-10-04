280995dacd15ab2c827096aa92265ac0c5176247
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-04 11:54:02 +0500

    modefied models to match json

--- src/TelegramBotApp/models/Search.cs
+++ src/TelegramBotApp/models/Search.cs
@@ -10,7 +10,7 @@ namespace TelegramBotApp.models
     public class Search
     {
         public string Title { get; set; }
-        public int Year { get; set; }
+        public string Year { get; set; }
         public string imdbID { get; set; }
         public string Type { get; set; }
         public string Poster { get; set; }
